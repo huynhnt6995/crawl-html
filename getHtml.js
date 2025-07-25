299adb0619ca3e340688d1e01273242373958272
huynhnt <huynhnt6995@gmail.com>
2025-07-25 21:04:41 +0700

    update

--- getHtml.js
+++ getHtml.js
@@ -46,7 +46,7 @@ async function getHtml(url, waitKey) {
 
   const page = await browser.newPage();
   await page.setUserAgent(
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
   );
   await page.goto(url);
 
